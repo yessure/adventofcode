043677d9eea22c9702086bfc3a03acab60fa54c6
teikakki <dafocabar@gmail.com>
2018-12-01 13:52:48 +0000

    Create 1_2.php

---
+++ 2018/1_2.php
@@ -0,0 +1,25 @@
+<?php
+$raw_input = trim(file_get_contents('input.txt'));
+$input = explode(PHP_EOL, $raw_input);
+$input = preg_replace('!\s+!', ' ', $input);
+
+foreach($input as &$space_row) {
+	$space_row = explode(" ", $space_row);
+}
+
+$result = 0;
+
+$numbers = array();
+foreach($input as &$pline) {
+
+	foreach($pline as &$pword) {
+		$result += $pword;
+		if(in_array($result, $numbers)) {
+			echo $result;
+			
+			return;
+		}
+		$numbers[] = $result;
+		$input[0][] = $pword;
+	}
+}
